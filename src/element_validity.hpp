c7c0910c4af2f834d2511b361f25a72f08967c5b
fsichetti <f.sichetti@gmail.com>
2024-08-20 12:33:44 +0200

    fixes

--- src/element_validity.hpp
+++ src/element_validity.hpp
@@ -1,3 +1,4 @@
 #pragma once
 #include "validity/StaticValidator.hpp"
 #include "validity/ContinuousValidator.hpp"
+#include "validity/JacobianEvaluator.hpp"
--- src/validity/JacobianEvaluator.hpp
+++ src/validity/JacobianEvaluator.hpp
@@ -1,26 +1,29 @@
 #pragma once
+#include "numbers/Interval.hpp"
 #include "utils/combinatorics.hpp"
 #include "lagrangeVector.hpp"
 #include "lagrangeEvaluate.hpp"
 
+namespace element_validity {
 template<uint n, uint s, uint p>
 class JacobianEvaluator {
 	private:
 	std::vector<Interval> coeffs;
-	constexpr uint numCoordsPerElem = nControlGeoMap(n,s,p) * n;
+	static constexpr uint numCoordsPerElem = nControlGeoMap(n,s,p) * n;
+	static constexpr uint numLagCoefPerElem = nControlJacobian(n,s,p,false);
 	const uint numEl = 0;
 
 	public:
 	JacobianEvaluator(const std::span<const fp_t>);
-}
+	fp_t eval(uint, std::span<const fp_t>) const;
+};
 
 template<uint n, uint s, uint p>
-JacobianEvaluator<n, s, p>(const std::span<const fp_t> cp) :
+JacobianEvaluator<n, s, p>::JacobianEvaluator(const std::span<const fp_t> cp) :
 	numEl(cp.size() / (numCoordsPerElem)
 ) {
 	const uint totCoord = numEl * numCoordsPerElem;
 	assert(cp.size() == totCoord);
-	const uint numLagCoefPerElem = nControlJacobian(n,s,p,false);
 	coeffs.resize(totCoord);
 	for (uint i=0; i < numEl; ++i) {
 		std::span<const fp_t> in =
@@ -35,8 +38,9 @@ JacobianEvaluator<n, s, p>(const std::span<const fp_t> cp) :
 template<uint n, uint s, uint p>
 fp_t JacobianEvaluator<n, s, p>::eval(uint i, std::span<const fp_t> x) const {
 	assert(x.size() == n);
-	std::span<const fp_t> lag(
+	std::span<const Interval> lag(
 		coeffs.data() + i * numLagCoefPerElem, numLagCoefPerElem);
-	Interval res = lagrangeEvaluate(x, lag);
+	Interval res = lagrangeEvaluate<n,s,p>(x, lag);
 	return res.middle();
+}
 }
