996cfc4d48b7dbf62b3b08d3110293a0d9c94c2f
fsichetti <f.sichetti@gmail.com>
2024-08-23 16:34:35 +0200

    temporarily disabled p4 tets

---
+++ src/utils/eigen.cpp
@@ -0,0 +1,31 @@
+#ifdef EIGEN_INTERFACE
+#include "eigen.hpp"
+
+namespace element_validity {
+std::vector<double> convertEigenMatrix(const Eigen::MatrixXd& mat) {
+	std::vector<double> vec;
+	vec.reserve(mat.size());
+	for (int i = 0; i < mat.rows(); ++i) {
+		for (int j = 0; j < mat.cols(); ++j) {
+			vec.push_back(mat(i, j));
+		}
+	}
+	return vec;
+}
+std::vector<double> convertEigenMatrix(
+	const Eigen::MatrixXd& mat1,
+	const Eigen::MatrixXd& mat2
+) {
+	std::vector<double> vec;
+	assert(mat1.rows() == mat2.rows() && mat1.cols() == mat2.cols());
+	vec.reserve(mat1.size() * 2);
+	for (int i = 0; i < mat1.rows(); ++i) {
+		for (int j = 0; j < mat1.cols(); ++j) {
+			vec.push_back(mat1(i, j));
+			vec.push_back(mat2(i, j));
+		}
+	}
+	return vec;
+}
+}
+#endif
